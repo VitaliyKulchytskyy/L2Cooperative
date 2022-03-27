7f0cf79d0ffa14bc32e8037c26f176f6b478e3d7
SunThy0 <sunthy13361@gmail.com>
2022-03-27 18:46:31 +0200

    I implemented code PartC_Block1.cs and PartC_Block3.cs with made changes. ^_^

--- PartC/PartC_Block1.cs
+++ PartC/PartC_Block1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace CooperativeLab
 {
@@ -6,8 +7,48 @@ namespace CooperativeLab
     {
         private const byte m_variant = 1;
 
+        private int FindFirstPairDigitInRow()
+        {
+            
+            for (int i = 0; i < Program.GArray.Length; i++) // made for searching of Pair digit itself in hole array
+            {
+                if (Program.GArray[i]%2==0) // when it should be found
+                {
+                    return i;
+                }
+                
+            }
+
+            return -1;
+            
+
+           
+
+            //return index; // the end
+
+        }
+
+
         public override void ProcessData()
         {
+            if (Program.GArray.Length == 0)
+                throw new Exception("NotEnoughValuesException");// end of exception
+            
+            int pairElement = FindFirstPairDigitInRow(); // pair element 
+            Console.WriteLine(pairElement);
+            if (pairElement == -1)
+                throw new Exception("BadResizeException"); // ISSUE
+                
+            
+
+            for (int i = pairElement + 1; i < Program.GArray.Length; i++) //when to resize 
+            {
+                Program.GArray[i - 1] = Program.GArray[i]; // how to resize
+            }
+
+            
+            Array.Resize(ref Program.GArray, Program.GArray.Length-1); // resize itself
+            
         }
 
         public override byte GetBlockVariant() => m_variant;
--- PartC/PartC_Block3.cs
+++ PartC/PartC_Block3.cs
@@ -6,8 +6,42 @@ namespace CooperativeLab
     {
         private const byte m_variant = 13;
 
+        
+        private byte RowMaxDigit()
+        {
+            int max = Program.GJArray[0][0];
+            byte index = 0;
+
+            for(byte i = 0; i < Program.GJArray.Length; i++)
+            {
+                for(byte j = 0; j < Program.GJArray[i].Length; j++)
+                {
+                    if(max <= Program.GJArray[i][j])
+                    {
+                        max = Program.GJArray[i][j];
+                        index = i;
+                    }
+                }
+            }
+
+            return index;
+        }
+
         public override void ProcessData()
         {
+            if (Program.GJArray.Length == 0 || Program.GJArray[0].Length == 0)
+                throw new Exception("NotEnoughValuesException");
+
+            byte minRow = RowMaxDigit();
+            Array.Resize(ref Program.GJArray, Program.GJArray.Length + 1);
+
+            for (int i = Program.GJArray.Length - 2; i >= minRow; i--)
+                Program.GJArray[i + 1] = Program.GJArray[i];
+
+            int[] emptyRow = new int[Program.GJArray[0].Length];
+            Array.Clear(emptyRow, 0, emptyRow.Length);
+
+            Program.GJArray[minRow] = emptyRow;
         }
 
         public override byte GetBlockVariant() => m_variant;
